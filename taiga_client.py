3af3151f98f79fa268cee11014e09e5fc9cf6c87
Igor Flint <netwamp@gmail.com>
2024-10-16 17:24:06 +0700

    Finished prototype

--- taiga_client.py
+++ taiga_client.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 import logging
-from pprint import pprint
 import json
 
 import requests
 
-import config, exceptions
-
+import config
 
 logger = logging.getLogger(__name__)
 
@@ -23,10 +21,10 @@ class URLConfig:
     def auth(self):
         return f"{self.__hostname}/api/v1/auth"
 
-    def tasks(self, project_id:int=None, milestone_id:int=None):
+    def tasks(self, project_id:int|None=None, milestone_id:int|None=None):
         base_url = f"{self.__hostname}/api/v1/tasks"
         if project_id or milestone_id:
-            base_url += "?{project_id=}&{milestone_id=}"
+            base_url += "?{project=}&{milestone=}"
         return base_url
 
 
@@ -48,38 +46,90 @@ class SessionStorage:
     def token(self, value):
         self.__data['token'] = value
 
+    @property
+    def tasks(self):
+        return self.__data.get('tasks', list())
+
+    @tasks.setter
+    def tasks(self, value):
+        self.__data['tasks'] = value
+
+    @property
+    def task_ids(self):
+        return set(self.__data.get('task_ids', set()))
+
+    @task_ids.setter
+    def task_ids(self, value):
+        self.__data['task_ids'] = list(value)
+
     def save(self):
         with open(self.__store_file_path, 'w') as store_file:
-            json.dump(self.__data, store_file)
+            data_copy = self.__data
 
+            json.dump(data_copy, store_file)
+
+
+class TaigaClient:
+    """
+    User-level abstraction interface to Taiga
+    """
+
+    def __init__(self, session:SessionStorage, urls:URLConfig):
+        self._session = session
+        self._urls = urls
+
+    def login(self):
+        if not self._session.token:
+            logger.debug("Receiving new token")
+            response = requests.post(
+                self._urls.auth, json={
+                    "username": config.LOGIN,
+                    "password": config.PASSWORD,
+                    "type": "normal",
+                })
+            response.raise_for_status()
+            if response.status_code == 200:
+                self._session.token = response.json()['auth_token']
+                self._session.save()
+                logger.debug("Done")
+            else:
+                logger.warn(f"Can't proceed due to authentication failure ({ response.status_code=})")
+                exit(0)
+        else:
+            logger.debug("Token taken from session")
+
+
+    def update_tasks(self) -> set[int]:
+        if not self._session.task_ids:
+            self._session.task_ids = self._retreive_task_ids()
+            self._session.save()
+            return {}
+        else:
+            all_ids = self._retreive_task_ids()
+            new_ids = all_ids - self._session.task_ids
+            self._session.task_ids = all_ids
+            self._session.save()
+            return new_ids
+
+    def _retreive_task_ids(self) -> set[int]:
+        result = set()
+        for milestone in config.TARGET_MILESTONES:
+            project_id = milestone['project_id']
+            milestone_id = milestone['milestone_id']
+            logger.debug(f'Trying to retreive project {project_id}, sprint {milestone_id}')
+            response = requests.get(
+                urls.tasks(project_id, milestone_id),
+                headers={'Authorization': f'Bearer {session.token}'}
+            )
+            response.raise_for_status()
+            result.update(set(map(lambda x: int(x['id']), response.json())))
+        return result
 
 if __name__ == '__main__':
     urls = URLConfig(config.TAIGA_HOST)
     session = SessionStorage()
+    client = TaigaClient(session, urls)
 
-    if not session.token:
-        logger.debug("Receiving new token")
-        response = requests.post(
-            urls.auth, json={
-                "username": config.LOGIN,
-                "password": config.PASSWORD,
-                "type": "normal",
-            })
-        response.raise_for_status()
-        if response.status_code == 200:
-            session.token = response.json()['auth_token']
-            session.save()
-            logger.debug("Done")
-        else:
-            logger.warn(f"Can't proceed due to authentication failure ({ response.status_code=})")
-            exit(0)
-    else:
-        logger.debug("Token taken from session")
-
-    for milestone in config.TARGET_MILESTONES:
-        project_id = milestone['project_id']
-        milestone_id = milestone['milestone_id']
-        logger.debug(f'Trying to retreive project {project_id}, sprint {milestone_id}')
-        response = requests.get(urls.tasks(project_id, milestone_id))
-        response.raise_for_status()
-        # pprint(response.json())
+    client.login()
+    new_ids = client.update_tasks()
+    print("New ids:", new_ids)
